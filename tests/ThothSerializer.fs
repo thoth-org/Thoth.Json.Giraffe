74a8b8561ecf7d211312202147f4611a3cd44e33
Maxime Mangel <mangel.maxime@protonmail.com>
2020-06-09 21:37:30 +0200

    Fix indentation in tests file

--- tests/ThothSerializer.fs
+++ tests/ThothSerializer.fs
@@ -27,12 +27,12 @@ type AlarmListViewModel = {
 }
 
 module Routes =
-  let root = "/"
-  let single = "/alarmListViewModel"
-  let nonSeq= "/nonseq"
-  let seq = "/seq"
-  let arraySeq = "/array"
-  let listSeq = "/list"
+    let root = "/"
+    let single = "/alarmListViewModel"
+    let nonSeq= "/nonseq"
+    let seq = "/seq"
+    let arraySeq = "/array"
+    let listSeq = "/list"
 
 module SampleSeq =
     open Thoth.Json.Net
@@ -60,26 +60,26 @@ module SampleSeq =
 // Test server/client setup
 // ----------------------------------------------------------------------------
 module Encode =
-  open Thoth.Json.Net
+    open Thoth.Json.Net
 
-  let person (id, name) =
-    Encode.object
-      [ "id",   Encode.int id
-        "name", Encode.string name
-      ]
+    let person (id, name) =
+      Encode.object
+        [ "id",   Encode.int id
+          "name", Encode.string name
+        ]
 
-  let persons ps = List.map person ps |> Encode.list
+    let persons ps = List.map person ps |> Encode.list
 
 module Decode =
-  open Thoth.Json.Net
+    open Thoth.Json.Net
 
-  let person =
-    Decode.map2 (fun id name -> id, name)
-      (Decode.field "id" Decode.int)
-      (Decode.field "name" Decode.string)
+    let person =
+        Decode.map2 (fun id name -> id, name)
+            (Decode.field "id" Decode.int)
+            (Decode.field "name" Decode.string)
 
-  let persons =
-    Decode.list person
+    let persons =
+        Decode.list person
 
 let webApp =
     let postAndSendBack next ctx =
@@ -90,22 +90,24 @@ let webApp =
         }
 
     choose
-      [
-        route Routes.root >=>
-          choose
-            [ GET >=>
-                ThothSerializer.RespondJson
-                  [ (1, "Maxime")
-                    (2, "Thoth")
-                  ]
-                  Encode.persons
-              POST >=> postAndSendBack
-            ]
-        route Routes.single >=> GET >=> ThothSerializer.RespondJson (SampleSeq.data.[0]) (Thoth.Json.Net.Encode.Auto.generateEncoder(SampleSeq.cs))
-        route Routes.nonSeq >=> GET >=> ThothSerializer.RespondJson (SampleSeq.data) (Thoth.Json.Net.Encode.Auto.generateEncoder(SampleSeq.cs))
-        route Routes.seq >=> GET >=> Thoth.Json.Giraffe.ThothSerializer.RespondJsonSeq (SampleSeq.data :> seq<_>) (Thoth.Json.Net.Encode.Auto.generateEncoder(SampleSeq.cs))
-        route Routes.arraySeq >=> GET >=> SampleSeq.respondJsonAutoArray SampleSeq.data
-        route Routes.listSeq >=> GET >=> SampleSeq.respondJsonAutoList SampleSeq.data2
+        [
+            route Routes.root >=>
+                choose
+                    [
+                        GET >=>
+                            ThothSerializer.RespondJson
+                                [
+                                    (1, "Maxime")
+                                    (2, "Thoth")
+                                ]
+                                Encode.persons
+                        POST >=> postAndSendBack
+                    ]
+            route Routes.single >=> GET >=> ThothSerializer.RespondJson (SampleSeq.data.[0]) (Thoth.Json.Net.Encode.Auto.generateEncoder(SampleSeq.cs))
+            route Routes.nonSeq >=> GET >=> ThothSerializer.RespondJson (SampleSeq.data) (Thoth.Json.Net.Encode.Auto.generateEncoder(SampleSeq.cs))
+            route Routes.seq >=> GET >=> Thoth.Json.Giraffe.ThothSerializer.RespondJsonSeq (SampleSeq.data :> seq<_>) (Thoth.Json.Net.Encode.Auto.generateEncoder(SampleSeq.cs))
+            route Routes.arraySeq >=> GET >=> SampleSeq.respondJsonAutoArray SampleSeq.data
+            route Routes.listSeq >=> GET >=> SampleSeq.respondJsonAutoList SampleSeq.data2
       ]
 
 let createHost () =
@@ -119,48 +121,50 @@ let createHost () =
 
 [<Tests>]
 let tests =
-  let json = "[{\"id\":1,\"name\":\"Maxime\"},{\"id\":2,\"name\":\"Thoth\"}]"
-  let createSeqTest name route =
-      testTask name {
-          let host = createHost ()
-          use _ = host.StartAsync ()
-          let client = new HttpClient()
-          let! (response : HttpResponseMessage) = client.GetAsync(sprintf "http://localhost:5000%s" route)
-          let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
-          Expect.equal content SampleSeq.expected <| sprintf "Serialization Seq - %s" name
-      }
+    let json = "[{\"id\":1,\"name\":\"Maxime\"},{\"id\":2,\"name\":\"Thoth\"}]"
+    let createSeqTest name route =
+        testTask name {
+            let host = createHost ()
+            use _ = host.StartAsync ()
+            let client = new HttpClient()
+            let! (response : HttpResponseMessage) = client.GetAsync(sprintf "http://localhost:5000%s" route)
+            let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
+            Expect.equal content SampleSeq.expected <| sprintf "Serialization Seq - %s" name
+        }
 
-  testList "ThothSerializer" [
-      testTask "Serialization" {
-          let host = createHost ()
-          use _ = host.StartAsync ()
-          let client = new HttpClient()
-          let! (response : HttpResponseMessage) = client.GetAsync("http://localhost:5000/")
-          let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
+    testList "ThothSerializer" [
+        testTask "Serialization" {
+            let host = createHost ()
+            use _ = host.StartAsync ()
+            let client = new HttpClient()
+            let! (response : HttpResponseMessage) = client.GetAsync("http://localhost:5000/")
+            let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
 
-          Expect.equal content json "Serialization failure"
-      }
-      testTask "SerializationAlarmListViewModel" {
-          let host = createHost ()
-          use _ = host.StartAsync ()
-          let client = new HttpClient()
-          let! (response : HttpResponseMessage) = client.GetAsync(sprintf "http://localhost:5000%s" Routes.single)
-          let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
+            Expect.equal content json "Serialization failure"
+        }
 
-          Expect.equal content SampleSeq.expectedSingle "Serialization failure"
-      }
-      createSeqTest "SerializationNonSeq" Routes.nonSeq
-      createSeqTest "SerializationSeq" Routes.seq
-      createSeqTest "SerializationArray" Routes.arraySeq
-      createSeqTest "SerializationList" Routes.listSeq
+        testTask "SerializationAlarmListViewModel" {
+            let host = createHost ()
+            use _ = host.StartAsync ()
+            let client = new HttpClient()
+            let! (response : HttpResponseMessage) = client.GetAsync(sprintf "http://localhost:5000%s" Routes.single)
+            let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
 
-      testTask "Deserialization" {
-          let host = createHost ()
-          use _ = host.StartAsync ()
-          let client = new HttpClient()
-          let! (response : HttpResponseMessage) = client.PostAsync("http://localhost:5000/", new StringContent(json))
-          let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
+            Expect.equal content SampleSeq.expectedSingle "Serialization failure"
+        }
 
-          Expect.equal content json "Deserialization failure"
-      }
+        createSeqTest "SerializationNonSeq" Routes.nonSeq
+        createSeqTest "SerializationSeq" Routes.seq
+        createSeqTest "SerializationArray" Routes.arraySeq
+        createSeqTest "SerializationList" Routes.listSeq
+
+        testTask "Deserialization" {
+            let host = createHost ()
+            use _ = host.StartAsync ()
+            let client = new HttpClient()
+            let! (response : HttpResponseMessage) = client.PostAsync("http://localhost:5000/", new StringContent(json))
+            let! content = response.EnsureSuccessStatusCode().Content.ReadAsStringAsync()
+
+            Expect.equal content json "Deserialization failure"
+        }
   ]
